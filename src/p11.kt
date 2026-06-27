986157e7693a21e87b176d9816b1809b9543bd83
Tirth Patel <your-email@example.com>
2026-06-27 12:39:37 +0530

    Practical_1 Practical_2

---
+++ src/p11.kt
@@ -0,0 +1,14 @@
+fun main(){
+    val i=10000
+    val j : Double=i.toDouble()
+    println("Integer value= $i")
+    println("Double value(From Integer)= $j")
+    val k:String="14"
+    println("String value= $k")
+    val l:Int=k.toInt()
+    println("Int value(From String)= $l")
+    val m:String="14.01"
+    println("String value= $m")
+    val a:Double=m.toDouble()
+    println("Double value(From String)= $a")
+}
---
+++ src/p12.kt
@@ -0,0 +1,18 @@
+fun main(){
+    val i=10000
+    val j:Double=i.toDouble()
+    val k="10"
+    val l:Int=k.toInt()
+    val m:Int=k.toInt()
+    val o="10.2"
+    val n:Double=o.toDouble()
+
+    println("i="+i)
+    println("j="+j)
+    println("k="+k)
+    println("l="+l)
+    println("m="+m)
+    println("o="+o)
+    println("n="+n)
+
+}
